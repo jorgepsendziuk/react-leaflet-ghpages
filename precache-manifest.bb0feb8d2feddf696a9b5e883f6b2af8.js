51e7bfb5eaf3c6059e593f94090b489797700c84
Jorge Psendziuk <jimxxx@gmail.com>
2021-01-27 09:29:58 -0400

    Updates

---
+++ precache-manifest.bb0feb8d2feddf696a9b5e883f6b2af8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf036f5e25b28f958a46f4e64c90592",
+    "url": "/react-leaflet-ghpages/index.html"
+  },
+  {
+    "revision": "d71de7b879f698dbeb91",
+    "url": "/react-leaflet-ghpages/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "01312ee4efabd831170a",
+    "url": "/react-leaflet-ghpages/static/css/main.45836c82.chunk.css"
+  },
+  {
+    "revision": "d71de7b879f698dbeb91",
+    "url": "/react-leaflet-ghpages/static/js/2.95e7176c.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/react-leaflet-ghpages/static/js/2.95e7176c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01312ee4efabd831170a",
+    "url": "/react-leaflet-ghpages/static/js/main.590cec47.chunk.js"
+  },
+  {
+    "revision": "e09f484b860c15bea04f",
+    "url": "/react-leaflet-ghpages/static/js/runtime-main.0a589734.js"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/react-leaflet-ghpages/static/media/venue_location_icon.2ceaf661.svg"
+  }
+]);
